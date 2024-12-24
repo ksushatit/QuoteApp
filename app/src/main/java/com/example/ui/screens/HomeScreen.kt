2fc99daafc20df2a20dfe81bbaccac5cefb535f6
ksushatit <ksusat096@gmail.com>
2024-12-24 23:45:35 +0300

    .

--- app/src/main/java/com/example/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/ui/screens/HomeScreen.kt
@@ -1,13 +1,20 @@
 package com.example.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.Env
 import com.example.viewmodels.HomeViewModel
 import com.example.viewmodels.getViewModel
 
 @Composable
-fun Env.HomeScreen() {
+fun Env.HomeScreen(modifier: Modifier = Modifier) {
     val viewModel = getViewModel { HomeViewModel(this) }
-    Text(viewModel.message)
+
+    Column(modifier.fillMaxSize()) {
+        Text(viewModel.message)
+        Text("balls")
+    }
 }
